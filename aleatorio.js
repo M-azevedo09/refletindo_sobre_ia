22d0ab4b166ab9ecf37a24fd3443053f06db1905
M-azevedo09 <mirela.azevedo.correa@escola.pr.gov.br>
2026-09-17 11:42:56 -0300

    Create aleatorio.js

---
+++ aleatorio.js
@@ -0,0 +1,9 @@
+const nomes = ["Hellena", "Fernanda", "Alice", "Maria Eduarda", "Luiza", "Vitória", "Amanda"];
+
+export function aleatorio (lista){
+  const posicao = Math.floor(Math.random()* lista.length);
+  return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
+
